59fab633b82d905dd54320339a00ee0e85bb7800
cdeboeuf <deboeuf.cecile@gmail.com>
2014-01-31 12:23:23 +0100

    Co

--- PhpProject1/annee.class.php
+++ PhpProject1/annee.class.php
@@ -1,26 +1,18 @@
 
 <?php
-    class annee {
+include('connexion.php');
+class annee 
+{     
     private $id;
     private $details;
     
     Public function LireAnnée()
     {
-        include('connexion.php');
-$bdd = new PDO('mysql:host=localhost;dbname=annee','root','') or die(print_r($bdd->errorInfo()));
-        $bdd->exec('SET NAMES utf8');        
-//connexion_annee();
-        //$connexion=new connexion();
-       // $connexion->connexion_annee();
+        $bdd=connexion_annee();
         $req="SELECT * FROM annee";
-       $ligne= $bdd->query($req);
-       // foreach($bdd->query($req) as $row){
-        //echo $row['0']." ".$row['1'];}
-
-//$bdd->prepare($req);
-        //$bdd->exec($bdd);
-        //$ligne=$bdd->fletchAll();
+        $ligne= $bdd->query($req);
         return $ligne;
     }
+ 
 }
 ?>
--- PhpProject1/connexion.php
+++ PhpProject1/connexion.php
@@ -1,7 +1,7 @@
 <?php
-class connexion{
+
 //Connexion à la base de donnée année.
-public static function connexion_annee()
+function connexion_annee()
 {
     try
     {
@@ -21,10 +21,14 @@ function connexion_base($annee)
     {
         $bdd = new PDO('mysql:host=localhost;dbname='.$annee.'','root','') or die(print_r($bdd->errorInfo()));
         $bdd->exec('SET NAMES utf8');
+        return $bdd;
+        
     }
     catch(Exception $e)
     {
         die('erreur: ' . $e->getMessage());
     }
-}}
+}
+
+    
 ?>
