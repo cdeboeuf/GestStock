1dc5d7d803064a447e5b96cbd8e80ed681be9f9a
cdeboeuf <deboeuf.cecile@gmail.com>
2014-01-31 11:46:30 +0100

    Co

---
+++ PhpProject1/connexion.php
@@ -0,0 +1,30 @@
+<?php
+class connexion{
+//Connexion à la base de donnée année.
+public static function connexion_annee()
+{
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname=annee','root','') or die(print_r($bdd->errorInfo()));
+        $bdd->exec('SET NAMES utf8');
+        return $bdd;
+    }
+    catch(Exception $e)
+    {
+        die('erreur: ' . $e->getMessage());
+    }
+}
+//Connexion à la base de donnée de l'année passer en parametre
+function connexion_base($annee)
+{
+    try
+    {
+        $bdd = new PDO('mysql:host=localhost;dbname='.$annee.'','root','') or die(print_r($bdd->errorInfo()));
+        $bdd->exec('SET NAMES utf8');
+    }
+    catch(Exception $e)
+    {
+        die('erreur: ' . $e->getMessage());
+    }
+}}
+?>
--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -1,13 +1,45 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <!--CSS -->
+        <link rel="stylesheet"  href="css/bootstrap-responsive.min.css">
+        <link rel="stylesheet"  href="css/bootstrap.min.css">
+        <!--responsive -->
+        <meta name="viewport" content="width=device-width,initial-scale=1.0">
     </head>
     <body>
-        tufvuvujh
-        <?php
-        // put your code here
-        ?>
+        <div class="container-fluid">
+            <div class="page-header">
+                <h1>
+                <small>Connexion</small></h1>
+            </div>
+            <div class="hero-unit"> 
+                <div class="span6"></div>
+                <form class="span3 ">
+                    <input type="text" placeholder="login">
+                    <input type="password" placeholder="mot de passe">
+                    <button type="submit" class="btn btn-primary">Envoyer</button>
+                </form>
+                <br>
+                <br>
+                <br>
+                <?php 
+                include('annee.class.php');
+                $annee=new annee();
+                $annee=$annee->LireAnnée();
+                foreach ($annee as $unMois)
+		{
+			    echo $unMois[1];
+                            echo " ";
+                }
+                ?>
+            </div>
+        </div>
+        <!--Js -->
+        <script src="http://code.jquery.com/jquery.js"></script>
+        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
+
